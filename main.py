1175f9e14eed36264370a855775845adae8482f2
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-07-11 20:55:45 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
-from sqlalchemy import  Column, Integer, String
-from sqlalchemy.orm import Session
-import psycopg2 # Иногда требуется для postgres
-# Бибилотеки SQL
+from SQLsession import SessionLocal
 
 from fastapi import Depends, FastAPI, Body
 from fastapi.responses import JSONResponse, FileResponse
 
-from decouple import Config, RepositoryEnv
-ENV_FILE = 'e.env'
-config = Config(RepositoryEnv(ENV_FILE))
-# Подключение конфиг-файла
 
-# Создаем модель бд
-
-class Base(DeclarativeBase): pass
-class Person(Base):
-   __tablename__ = config('TABLE_NAME')
-
-   id = Column(Integer, primary_key=True, index=True)
-   name = Column(String)
-   surname = Column(String)
-   birthday = Column(Integer)
-   status = Column(String)
-
-engine = create_engine(config('DB_LINK'))
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine) # Создание таблиц, если их нет
- 
-# API + описание 4-х его действий
+# API + описание 4-х действий
 app = FastAPI()
 
 def get_db():
